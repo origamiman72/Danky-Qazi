9d4e0a52e11ac8c51df1b425be681061ef56a672
origamiman72 <ahujaabhik@gmail.com>
2017-03-31 13:11:47 -0700

    update new files

---
+++ core/src/com/mygdx/game/MouseAdapter.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import java.awt.event.MouseListener;
+
+/**
+ * Created by ahuja on 3/31/2017.
+ */
+public abstract class MouseAdapter implements MouseListener{
+    public MouseAdapter(){
+
+    }
+}
